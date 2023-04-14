62608d38cfd5a4787178a506dc32fed338e57bc1
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-04-14 14:35:53 +0700

    Контроллеры и Dto для работы с пользователями

---
+++ src/main/java/ru/skypro/homework/controller/UserController.java
@@ -0,0 +1,168 @@
+package ru.skypro.homework.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.dto.NewPassword;
+import ru.skypro.homework.dto.User;
+
+@RestController
+@RequestMapping("/users")
+@Tag(name = "Пользователи")
+//@CrossOrigin(value = "http://localhost:3000")
+public class UserController {
+
+
+    @Operation(
+            summary = "Изменение пароля",
+            description = "Изменение пароля пользователя из тела запроса"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK. Установлен новый пароль",
+
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = NewPassword.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found",
+                    content = {@Content()}
+            )
+    }
+    )
+    @PostMapping("/set_password")
+    public ResponseEntity<NewPassword> setPassword(@RequestBody NewPassword passwords) {
+        NewPassword newPassword = new NewPassword();
+        return ResponseEntity.ok().body(newPassword);
+    }
+
+
+    @Operation(
+            summary = "Получение данных пользователя"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK. Данные пользователя получены",
+
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = User.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found",
+                    content = {@Content()}
+            )
+    }
+    )
+    @GetMapping("/me")
+    public ResponseEntity<User> getUser() {
+        User user = new User();
+        return ResponseEntity.ok().body(user);
+    }
+
+
+    @Operation(
+            summary = "Изменение данных пользователя",
+            description = "Обновление данных пользователя из тела запроса"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK. Данные пользователя обновлены",
+
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = User.class))
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "No Content",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Forbidden",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found",
+                    content = {@Content()}
+            )
+    }
+    )
+    @PatchMapping("/me")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+        User newuser = new User();
+        return ResponseEntity.ok().body(newuser);
+    }
+
+    @Operation(
+            summary = "Изменение изображения пользователя",
+            description = "Обновление изображения пользователя из тела запроса"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "OK. Изображение пользователя обновлено",
+                    content = {@Content()}
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Not Found",
+                    content = {@Content()}
+            )
+    }
+    )
+    @PatchMapping(value ="/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> updateUserImage(@RequestBody MultipartFile image) {
+        return ResponseEntity.ok().build();
+    }
+}
