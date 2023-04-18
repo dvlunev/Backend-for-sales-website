eebf9a7fc97a5e1866590dd66c13338c82e0ccc6
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-04-18 17:08:09 +0700

    поле adId с маленькой буквы

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -2,7 +2,7 @@ package ru.skypro.homework.entity;
 
 public class Comment {
     private int id;
-    private int AdId; //id объявления
+    private int adId; //id объявления
     private int authorId; //id автора комментария
     private String createdAt; //дата и время создания комментария в миллисекундах с 00:00:00 01.01.1970
     private String text;
