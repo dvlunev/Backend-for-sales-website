da23f1c430ec01b4a0e67c788b91606ffb7b591b
dvlunev <dvlunev@ya.ru>
2023-04-20 17:27:24 +0300

    Исправил контроллеры и DTO под новый файлик

--- src/main/java/ru/skypro/homework/dto/UserDto.java
+++ src/main/java/ru/skypro/homework/dto/UserDto.java
@@ -8,10 +8,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    //id пользователя
     private int id;
+    //логин пользователя
     private String email;
+    //имя пользователя
     private String firstName;
+    //фамилия пользователя
     private String lastName;
+    //телефон пользователя
     private String phone;
+    //ссылка на аватар пользователя
     private String image;
 }
