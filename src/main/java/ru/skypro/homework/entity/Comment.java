e202c32c9e6d655819aef1da6d82e88b8960f066
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-04-20 20:51:26 +0700

    Подключила liquibase, написала xml файлы для создания таблиц

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -24,7 +24,7 @@ public class Comment {
     @JoinColumn(name = "author_id", nullable = false)
     private User author;
 
-    @Column(name = "ad_date", length = 25, nullable = false)
+    @Column(name = "ad_date", length = 20, nullable = false)
     private String createdAt; //дата и время создания комментария в миллисекундах с 00:00:00 01.01.1970
 
     @Column(length = 1000, nullable = false)
