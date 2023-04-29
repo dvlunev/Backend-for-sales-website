6d7a6dbe2318847eab4b23d88cb32f386dc9dbd8
MoXuT0 <DenisFisenko2010@yandex.ru>
2023-04-29 12:23:07 +0300

    NOT FOUND

--- src/main/java/ru/skypro/homework/exception/UserNotFoundException.java
+++ src/main/java/ru/skypro/homework/exception/UserNotFoundException.java
@@ -3,6 +3,6 @@ package ru.skypro.homework.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NO_CONTENT)
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
 }
