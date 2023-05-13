298b79dafc5f57828c4ad5b561129da0b6f4d7ac
TamaraZolotovskaya <116792070+TamaraZolotovskaya@users.noreply.github.com>
2023-05-13 14:33:12 +0700

    Ещё тесты для UserService

--- src/test/java/ru/skypro/homework/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/skypro/homework/service/impl/UserServiceImplTest.java
@@ -1,25 +1,26 @@
 package ru.skypro.homework.service.impl;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import ru.skypro.homework.dto.UserDto;
 import ru.skypro.homework.entity.Image;
 import ru.skypro.homework.entity.User;
-import ru.skypro.homework.repository.ImageRepository;
 import ru.skypro.homework.repository.UserRepository;
+import ru.skypro.homework.service.ImageService;
 import ru.skypro.homework.service.mapper.UserMapper;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static ru.skypro.homework.service.UserServiceTestConstants.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,16 +30,14 @@ class UserServiceImplTest {
     @Mock
     UserRepository userRepository;
     @Mock
-    ImageRepository imageRepository;
+    ImageService imageService;
     @Mock
     private Authentication authentication;
     @Mock
     private SecurityContext securityContext;
-    @Mock
-    Image image;
     @InjectMocks
     UserServiceImpl userService;
-    private final User expectedUser = new User();
+    private User expectedUser = new User();
 
     @BeforeEach
     public void setUp() {
@@ -76,13 +75,11 @@ class UserServiceImplTest {
         verify(userRepository).findByEmail(USERNAME);
         verify(userMapper).mapToUserDto(expectedUser);
         assertEquals(expectedUserDto, actual);
-
-
     }
 
     @Test
-    void updateUserDto(){
-       User newUser = new User();
+    void updateUserDto() {
+        User newUser = new User();
         UserDto newUserDto = new UserDto();
         newUser.setId(1);
         newUser.setEmail(USERNAME);
@@ -100,5 +97,35 @@ class UserServiceImplTest {
         verify(userRepository).findByEmail(USERNAME);
         assertEquals(newUserDto, actual);
     }
+
+    @Test
+    @DisplayName("Обновление фотографии пользователя, когда она уже существует")
+    void updateUserImageWhenOldImageIsNotNull() {
+        Image imageOld = new Image("imageOld", new byte[20]);
+        Image imageNew = new Image("imageNew", new byte[20]);
+        expectedUser.setImage(imageOld);
+        MockMultipartFile newImageFile = new MockMultipartFile("imageNew", new byte[30]);
+
+        when(imageService.updateImage(newImageFile, expectedUser.getImage())).thenReturn(imageNew);
+        userService.updateUserImage(newImageFile);
+
+        verify(imageService, times(1)).updateImage(newImageFile, imageOld);
+        verify(userRepository, times(1)).save(expectedUser);
+        assertEquals(imageNew, expectedUser.getImage());
+    }
+
+    @Test
+    @DisplayName("Обновление фотографии пользователя в случае, если её не было")
+    void updateUserImageWhenOldImageIsNull() {
+        Image imageNew = new Image("imageNew", new byte[20]);
+        MockMultipartFile newImageFile = new MockMultipartFile("imageNew", new byte[30]);
+        when(imageService.saveImage(newImageFile)).thenReturn(imageNew);
+
+        userService.updateUserImage(newImageFile);
+        verify(imageService, times(1)).saveImage(newImageFile);
+        verify(userRepository, times(1)).save(expectedUser);
+        assertEquals(imageNew, expectedUser.getImage());
+    }
+
 }
 
