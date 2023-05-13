696490ac9fe098b25c943dbb7260299e779a9c86
salva <salvationordeath@gmail.com>
2023-05-13 23:38:44 +0700

    Подправила тесты

--- src/test/java/ru/skypro/homework/security/BasicAuthCorsFilterTest.java
+++ src/test/java/ru/skypro/homework/security/BasicAuthCorsFilterTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import ru.skypro.homework.security.BasicAuthCorsFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
